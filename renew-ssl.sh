db6cae8b1343548ed2b03a2c89ba3af35929d2dd
Jason <quietjosen@gmail.com>
2023-12-02 11:54:24 +0800

    Add comment

--- renew-ssl.sh
+++ renew-ssl.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# sudo docker compose run --rm certbot certonly --email demo@gmail.com --agree-tos --no-eff-email --webroot --webroot-path /home/www/hz.fenj.vip -d hz.fenj.vip
+
 sudo docker compose run --rm certbot renew
 
